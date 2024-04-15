0bd34447fcb1f6fa4851d336f01809482c8b4de4
unknown <malibu1106@gmail.com>
2024-04-15 23:09:38 +0200

    32 cartes ok - need commenter + compteur tentatives + reglages difficulté + image au lieu de couleur

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let cardNumberPairs = 6; // Nombre de paires de cartes
-let colors = ['red', 'green', 'yellow', 'blue', 'purple', 'orange']; // liste des couleurs
+let cardNumberPairs = 16; // Nombre de paires de cartes
+let colors = ['red', 'green', 'yellow', 'blue', 'purple', 'orange', 'pink', 'teal', 'black', 'silver', 'gray', 'white', 'maroon', 'lime', 'olive', 'fuchsia']; // liste des couleurs
 
 function createCardDiv(color, id) { //fonction pour créer les cartes
     let div = document.createElement("div"); // on créé une div
@@ -9,26 +9,41 @@ function createCardDiv(color, id) { //fonction pour créer les cartes
     document.getElementById('conteneur-cartes').appendChild(div); // push la carte dans mon conteneur
 }
 
-function cardColorPicker() { // attribution des couleurs aux cartes
-    let colorIndex = 0; // Indice de la couleur actuellement attribuée
+function cardColorPicker() {
+    let colorIndex = 0;
+    let colorCount = 0; // Nombre de fois qu'une couleur a été utilisée
     id = 1;
-    for (let i = 0; i < cardNumberPairs; i++) { // pour le nomber de paires
-        let color = colors[colorIndex]; // Prend la couleur actuelle dans le tableau colors
+
+    for (let i = 0; i < cardNumberPairs; i++) {
+        let color = colors[colorIndex];
         createCardDiv(color, id);
         id++;
-        createCardDiv(color, id); // Crée deux cartes avec la même couleur
+        createCardDiv(color, id);
         id++;
-        colorIndex = (colorIndex + 1) % colors.length; // Passe à la couleur suivante demander à Erwann pk un modulo etc oO'
+
+        colorCount++; // Incrémentez le nombre d'utilisations de la couleur actuelle
+
+        // Si une couleur a été utilisée autant de fois que nécessaire, passez à la couleur suivante
+        if (colorCount >= Math.ceil(cardNumberPairs / colors.length)) {
+            colorIndex = (colorIndex + 1) % colors.length;
+            colorCount = 0; // Réinitialisez le nombre d'utilisations de la couleur actuelle
+        }
     }
 }
 
 cardColorPicker();// On appelle la fonction pour définir les couleurs des cartes
 
-function shuffleCards() { // mélange des cartes
-    let cards = document.querySelectorAll('.card'); // variable avec toutes les cartes
-    for (let i = cards.length - 1; i > 0; i--) { // longueur totale du tableau > parcoure tous les éléments + décrémente
-        let j = Math.floor(Math.random() * (i + 1)); // pas bien compris
-        cards[i].parentNode.insertBefore(cards[j], cards[i]); // pas bien compris
+function shuffleCards() {
+    let cards = document.querySelectorAll('.card');
+
+    // Convertissez NodeList en tableau pour faciliter le mélange
+    cards = Array.from(cards);
+
+    // Mélangez les cartes en utilisant l'algorithme de Fisher-Yates
+    for (let i = cards.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [cards[i], cards[j]] = [cards[j], cards[i]]; // Échangez les cartes de position
+        cards[i].parentNode.insertBefore(cards[i], cards[i].parentNode.firstChild); // Placez les cartes mélangées au début du conteneur
     }
 }
 shuffleCards(); //on appelle la fonction pour mélanger les cartes
@@ -58,8 +73,7 @@ function chosenCard(id) {
             chosenCard2 = "temp";
             firstOrSecondCard = 0;
             points++;
-            document.getElementById("success").innerHTML = points;
-            if (points > 5) { document.getElementById("success").innerHTML = "BRAVO"; }
+            if (points == cardNumberPairs) { document.getElementById("success").innerHTML = "BRAVO"; }
         }
         else {
             setTimeout(() => { hideCards(); }, "1500");
@@ -75,6 +89,7 @@ function chosenCard(id) {
 
 
 function hideCards() {
+
     document.getElementsByClassName(chosenCard1)[0].style.backgroundImage = "url(test.png)";
     document.getElementsByClassName(chosenCard1)[1].style.backgroundImage = "url(test.png)";
     document.getElementsByClassName(chosenCard2)[0].style.backgroundImage = "url(test.png)";
@@ -82,4 +97,5 @@ function hideCards() {
     chosenCard1 = "temp";
     chosenCard2 = "temp";
     firstOrSecondCard = 0;
+
 }
